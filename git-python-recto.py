b2317af1cffface905d09f434b42721f470b7a63
RectoThreena <rectothreena@gmail.com>
2022-03-11 13:53:04 +0800

    My Educational Background added

--- git-python-recto.py
+++ git-python-recto.py
@@ -1,3 +1,8 @@
 print("Threena rejano Recto")
 print("Brgy.Pag-Asa, Santa Cruz, Marinduque")
 print("January 23, 2002")
+
+#My Educational Background
+print("Marinduque State College")
+print("BS Information Technology")
+print("2A")
