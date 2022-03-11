9b72c60488adf0d9496df72f21cfea97b0ab69ac
RectoThreena <rectothreena@gmail.com>
2022-03-11 14:10:06 +0800

    My Hobbies added

--- git-python-recto.py
+++ git-python-recto.py
@@ -6,3 +6,10 @@ print("January 23, 2002")
 print("Marinduque State College")
 print("BS Information Technology")
 print("2A")
+
+#My Hobbies
+print("Reading Wattpad")
+print("Watching K-drama")
+print("Listening to music")
+print("Traveling")
+print("Cooking")
